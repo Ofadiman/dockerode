36e925eb993e2fffe88eb95e77d3c61c6c708153
ofadiman <ofadiman@gmail.com>
2024-12-01 18:10:58 +0100

    chore: delete legacy files

--- node/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { MainModule } from './main.module.js';
-
-void (async () => {
-  const app = await NestFactory.create(MainModule);
-  app.enableShutdownHooks();
-  await app.listen(3000);
-})();
